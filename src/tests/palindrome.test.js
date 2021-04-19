12e17addecda5bba9b50b67cfd74527e06e1f45a
Sebastian De Vita <sebassdevita@gmail.com>
2021-04-19 11:57:19 -0300

    Implementados ejemplos de testing

---
+++ src/tests/palindrome.test.js
@@ -0,0 +1,21 @@
+const palindrome = require("../utils/for_testing").palindrome
+
+describe.skip('palindrome', () => {
+  test("of a", () => {
+    const result = palindrome("a")
+  
+    expect(result).toBe("a")
+  })
+  
+  test("of sebasdev", () => {
+    const result = palindrome("sebasdev")
+  
+    expect(result).toBe("vedsabes")
+  })
+  
+  test("of releveler", () => {
+    const result = palindrome("releveler")
+  
+    expect(result).toBe("releveler")
+  }) 
+});
