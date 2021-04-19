aac1e14827308d302470be4ffc469f8db601e557
Sebastian De Vita <sebassdevita@gmail.com>
2021-04-19 09:43:58 -0300

    Separado controller en un module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http")
 const express = require("express")
-const app = express()
+const app = require('./app')
 const cors = require("cors")
 const mongoose = require("mongoose")
 
@@ -8,24 +8,8 @@ const Blog = require('./src/models/blog')
 const logger = require('./src/utils/logger')
 const config = require('./src/utils/config')
 
-
-
 app.use(cors())
 app.use(express.json())
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 PORT = config.PORT
 app.listen(PORT, () => {
