5a7966da8c8d2c27f6735d5ef91494b0192e73e0
Sebastian De Vita <sebassdevita@gmail.com>
2021-07-06 15:16:38 -0300

    Terminado ejercicio 4.15

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,15 +3,16 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
-  const body = request.body
+usersRouter.post('/', async (request, response, next) => {
+  const { body } = request
+  const { username, name, password } = body
 
   const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
   const user = new User({
-    username: body.username,
-    name: body.name,
+    username,
+    name,
     passwordHash
   })
 
