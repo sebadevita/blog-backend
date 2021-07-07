f0e4b61dfe8cd9e92383867b4467796c73d40e3f
Sebastian De Vita <sebassdevita@gmail.com>
2021-07-07 13:05:19 -0300

    Terminados tests de post users

--- src/tests/users_api.test.js
+++ src/tests/users_api.test.js
@@ -50,6 +50,35 @@ describe('get users', () => {
   })
 })
 
+describe('create user', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user1 = new User(initialUsers[0])
+    await user1.save()
+    const user2 = new User(initialUsers[1])
+    await user2.save()
+  })
+
+  test('a valid user can be added', async () => {
+    const newUser = {
+      username: 'newuser',
+      name: 'newname',
+      password: 'password123'
+
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/users')
+
+    expect(response.body).toHaveLength(initialUsers.length + 1)
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
